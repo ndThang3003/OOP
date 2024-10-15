7f66f2342c545604a365d60aaa8151d2c770e88d
ndThang3003 <thangnd.24ite@vku.udn.vn>
2024-10-15 21:32:07 +0700

    init project

---
+++ circle12.java
@@ -0,0 +1,28 @@
+public class circle12 {
+    private double radius = 1.0;
+    public circle12() {
+
+    }
+    public circle12(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
--- main14.java
+++ main14.java
@@ -1,4 +1,4 @@
-public class main02 {
+public class main14 {
     public static void main(String[] args) {
         Employee e1 = new Employee(95, "thắng", "nguyễn",10000000);
         System.out.println(e1);
@@ -8,6 +8,5 @@ public class main02 {
         System.out.println("name is: " + e1.getName());
         System.out.println("annualSalary is: " + e1.getAnnualSalary());
         System.out.println(e1.raiseSalary(10));
-        System.out.println(e1);
     }
 }
